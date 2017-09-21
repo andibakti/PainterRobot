a6e1ab3789d99ebf60427d8f203c2da94c920277
cclin130 <cheng.lin103@gmail.com>
2017-09-20 21:56:38 -0400

    finalized DC Motor Base Code (ready for manipulation and interfacing with java)

--- Arduino IDE/DC-Motor-Base-Code/DC-Motor-Base-Code.ino
+++ Arduino IDE/DC-Motor-Base-Code/DC-Motor-Base-Code.ino
@@ -9,13 +9,9 @@ int PWM_1 = A1; //pin to control motor 1's speed (analog)
 int DIR_2 = 2; //pin to control motor 2's direction (digital)
 int PWM_2 = A2; //pin to control motor 2's speed (analog)
 
-//declare int to control motor speed
-//(value will be the pulse width of the PWM, 0-255)
-int speed = 255;
-
-//integer for delay so motors don't start at the same time
-//(Arduino doesn't have enough current to start both at same time)
-int count = 0;
+//declare int as factor for converting degrees to duration of turn (in milliseconds)
+//factor will be determined through testing
+int factor = 10;
 
 //------------setup-------------------------------
 void setup() {
@@ -26,23 +22,92 @@ void setup() {
   //PWM pins do not need to be set as output to be used
 }
 
-/*/----------create methods to run motors-----------
-//we will construct the robot so a LOW digital output is forward
-//and a HIGH digital output is backward
+//----------create methods to run motors-----------
+void motorDelay(){
+  
+}
+//we will construct the robot so
+//LOW digital output is forward for M1
+//HIGH digital output is forward for M2
 
-void M1_forward (){
-  digitalWrite(DIR_1, LOW);
+//method to go forward, method accepts amount of time to travel forward
+void forward (int count){
+  analogWrite(PWM_1, 255); //set speed to max
+  digitalWrite(DIR_1, LOW); //LOW means M1 goes forward
+
+  //delay because Arduino does not have enough current to start both at same time
+  delay(100);
+  
+  analogWrite(PWM_2, 255); //set speed to max
+  digitalWrite(DIR_2, HIGH);
+
+  delay(count);
+  motorStop();
+}
+//method to go backward, method accepts amount of time to travel backward
+void backward(int count) {
+  
   analogWrite(PWM_1, 255);
-}*/
+  digitalWrite(DIR_1, HIGH); //HIGH means M1 goes backward
 
+  //delay because Arduino does not have enough current to start both at same time
+  delay(100);
+  
+  analogWrite(PWM_2, 255);
+  digitalWrite(DIR_2, LOW); //LOW means M2 goes backward
+
+  delay(count);
+  motorStop();
+}
+
+//method to turn right, accepts a degree value for the magnitude of the turn
+void rightTurn(int deg) {
+  //right turns require M1 to go forward while M2 backward
+  //motor 1 goes forward
+  analogWrite(PWM_1, 255); //set speed to max
+  digitalWrite(DIR_1, LOW); //LOW means M1 goes forward
+
+  //delay because Arduino does not have enough current to start both at same time
+  delay (100);
+
+  //motor 2 goes backward
+  analogWrite(PWM_2, 255); //set speed
+  digitalWrite(DIR_2, LOW); //LOW means M2 goes backward
+
+  delay(factor*deg);
+  motorStop();
+}
+
+//method to turn left, accepts a degree value for the magnitude of the turn
+void leftTurn(int deg) {
+  //left turns require M2 to go forward while M1 backward
+  //motor 2 goes forward
+  analogWrite(PWM_2, 255); //set speed to max
+  digitalWrite(DIR_2, HIGH); //LOW means M2 goes forward
+
+  //delay because Arduino does not have enough current to start both at same time
+  delay (100);
+
+  //motor 1 goes backward
+  analogWrite(PWM_1, 255); //set speed
+  digitalWrite(DIR_1, HIGH); //LOW means M2 goes backward
+
+  delay(factor*deg);
+  motorStop();
+}
+
+//stop method, stops motors for 1 second
+void motorStop() {
+  analogWrite(PWM_1, 0);
+  analogWrite(PWM_2, 0);
+  delay(1000); //stop motor for one second
+}
 
 //-----------loop----------------------------------
 void loop() {
-  analogWrite(PWM_1, 255);
-
-  if (count == 0) {
-    delay(100);
-    count++;
-  }
-  analogWrite(PWM_2, 255);
+  forward(1000);
+  backward(1000);
+  rightTurn(180);
+  leftTurn(180);
+  
 }
